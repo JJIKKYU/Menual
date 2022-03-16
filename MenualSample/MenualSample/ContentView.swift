e8335ee32e42e8295a0a22ef2ec14b983c4dfb2b
Jin Gyun, Jeong <jjinggu96@gmail.com>
2022-03-16 16:08:52 +0900

    ADD : SwiftUI

---
+++ MenualSample/MenualSample/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MenualSample
+//
+//  Created by 정진균 on 2022/03/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("asd")
+            .bold()
+            .foregroundColor(.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MenualSample/MenualSample/MenualSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenualSampleApp.swift
+//  MenualSample
+//
+//  Created by 정진균 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct MenualSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
