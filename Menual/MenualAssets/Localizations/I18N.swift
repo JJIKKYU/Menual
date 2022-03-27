3f167f83080b0ee88970fd5a953ad336e7619c39
Jin Gyun, Jeong <jjinggu96@gmail.com>
2022-03-27 21:14:40 +0900

    UPDATE : Localizations

--- Menual/MenualAssets/Localizations/I18N.swift
+++ Menual/MenualAssets/Localizations/I18N.swift
@@ -10,6 +10,7 @@ struct MenualString {
 
     // MENUAL UX Localizations 적용 버전 = 1.01
     static let localizations_ver = "localizations_ver".localized
+    static let title_menual = "title_menual".localized
     static let title_moments = "title_moments".localized
     static let title_menualList = "title_menualList".localized
     static let button_writeMenual = "button_writeMenual".localized
@@ -20,4 +21,7 @@ struct MenualString {
     static let placeholder_search = "placeholder_search".localized
     static let title_searchResult = "title_searchResult".localized
     static let title_searchHistory = "title_searchHistory".localized
+    static let title_showWeather = "title_showWeather".localized
+    static let title_showPlace = "title_showPlace".localized
+    static let title_filter = "title_filter".localized
 }
