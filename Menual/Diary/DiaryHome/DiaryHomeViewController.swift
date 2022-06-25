0ec85e70bf1bba71069fa0e5df59858dfb0028b1
Jin Gyun, Jeong <jjinggu96@gmail.com>
2022-06-25 15:20:29 +0900

    MyPage 초안 추가

--- Menual/Diary/DiaryHome/DiaryHomeViewController.swift
+++ Menual/Diary/DiaryHome/DiaryHomeViewController.swift
@@ -117,25 +117,6 @@ final class DiaryHomeViewController: UIViewController, DiaryHomePresentable, Dia
         $0.tag = -1
     }
     
-    var isStickyMyMenualCollectionView: Bool = false
-    let isEnableStickyHeaderYOffset: CGFloat = 360 // StickHeader가 작동해야 하는 yOffset Value
-    lazy var myMenualCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-        let flowlayout = UICollectionViewFlowLayout.init()
-        flowlayout.scrollDirection = .horizontal
-        flowlayout.minimumLineSpacing = 10
-        flowlayout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        $0.setCollectionViewLayout(flowlayout, animated: true)
-        $0.backgroundColor = .clear
-        $0.delegate = self
-        $0.dataSource = self
-        $0.register(TabsCell.self, forCellWithReuseIdentifier: "TabsCell")
-        $0.register(DividerView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "DividerView")
-        $0.tag = 1
-        $0.showsHorizontalScrollIndicator = false
-        $0.contentInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 20)
-    }
-    
     private let divider = Divider(type: ._2px).then {
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
@@ -159,14 +140,10 @@ final class DiaryHomeViewController: UIViewController, DiaryHomePresentable, Dia
     }
     
     func setViews() {
-//        title = MenualString.title_menual
-//        navigationItem.leftBarButtonItem = leftBarButtonItem
-//        navigationItem.rightBarButtonItem = rightBarButtonItem
         self.view.backgroundColor = Colors.background
         
         self.view.addSubview(naviView)
-        // self.view.addSubview(scrollView)
-         self.view.addSubview(fabWriteBtn)
+        self.view.addSubview(fabWriteBtn)
         
         self.view.addSubview(myMenualTableView)
         myMenualTableView.tableHeaderView = tableViewHeaderView
@@ -174,7 +151,6 @@ final class DiaryHomeViewController: UIViewController, DiaryHomePresentable, Dia
         tableViewHeaderView.addSubview(momentsCollectionView)
         tableViewHeaderView.addSubview(momentsCollectionViewPagination)
         tableViewHeaderView.addSubview(myMenualTitleView)
-        tableViewHeaderView.addSubview(myMenualCollectionView)
         tableViewHeaderView.addSubview(divider)
         tableViewHeaderView.addSubview(myMenualPageTitleView)
         
@@ -233,17 +209,10 @@ final class DiaryHomeViewController: UIViewController, DiaryHomePresentable, Dia
             make.height.equalTo(22)
         }
         
-        myMenualCollectionView.snp.makeConstraints { make in
-            make.leading.equalToSuperview()
-            make.width.equalToSuperview()
-            make.top.equalTo(myMenualTitleView.snp.bottom).offset(12)
-            make.height.equalTo(60)
-        }
-        
         myMenualPageTitleView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.width.equalToSuperview()
-            make.top.equalTo(myMenualCollectionView.snp.bottom).offset(16)
+            make.top.equalTo(myMenualTitleView.snp.bottom).offset(16)
             make.height.equalTo(24)
         }
         
@@ -316,66 +285,9 @@ final class DiaryHomeViewController: UIViewController, DiaryHomePresentable, Dia
 // MARK: - Scroll View
 extension DiaryHomeViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        // stickyMyMenualCollectionView(scrollView)
         momentsPagination(scrollView)
     }
-    
-    // MyMenualCollectionView보다 아래로 스크롤 했을 경우 상단에 고정되도록
-    func stickyMyMenualCollectionView(_ scrollView: UIScrollView) {
-        // CollectionView 위치보다 아래로 갈 경우 (StickyHeader 작동)
-        if scrollView.contentOffset.y > self.isEnableStickyHeaderYOffset {
-            if isStickyMyMenualCollectionView { return }
-            self.myMenualCollectionView.snp.remakeConstraints { make in
-                make.leading.equalToSuperview()
-                make.width.equalToSuperview()
-                make.height.equalTo(100)
-                make.top.equalTo(self.view.snp.top)
-            }
-            self.view.layoutIfNeeded()
-            
-            // fabViewBtn 축소
-            // TODO: 따로 함수로 구현할 것
-            self.fabWriteBtn.snp.remakeConstraints { make in
-                make.trailing.equalToSuperview().inset(20)
-                make.height.equalTo(50)
-                make.width.equalTo(50)
-                make.bottom.equalToSuperview().inset(34)
-            }
-            UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseInOut) {
-                self.fabWriteBtn.layer.cornerRadius = 25
-                self.view.layoutIfNeeded()
-            }
-            print("StickyHeader 작동!")
-            isStickyMyMenualCollectionView = true
-        }
-        // StickyHeader가 작동하지 않아도 되는 경우 (평상시)
-        else {
-            if !isStickyMyMenualCollectionView { return }
-            self.myMenualCollectionView.snp.remakeConstraints { make in
-                make.leading.equalToSuperview()
-                make.width.equalToSuperview()
-                make.height.equalTo(100)
-                make.top.equalTo(myMenualTitleView.snp.bottom).offset(20)
-            }
-            self.view.layoutIfNeeded()
-            
-            // fabViewBtn 확장
-            // TODO: 따로 함수로 구현할 것
-            self.fabWriteBtn.snp.remakeConstraints { make in
-                make.leading.equalToSuperview().offset(20)
-                make.trailing.equalToSuperview().inset(20)
-                make.height.equalTo(50)
-                make.bottom.equalToSuperview().inset(34)
-            }
-            UIView.animate(withDuration: 0.75, delay: 0, options: .curveEaseInOut) {
-                self.fabWriteBtn.AppCorner(.tiny)
-                self.view.layoutIfNeeded()
-            }
-            print("StickyHeader 미작동!")
-            isStickyMyMenualCollectionView = false
-        }
-    }
-    
+
     func momentsPagination(_ scrollView: UIScrollView) {
         // MomentCollectionView 일때만 작동 되도록
         if scrollView.tag == 0 {
@@ -439,7 +351,6 @@ extension DiaryHomeViewController: UITableViewDelegate, UITableViewDataSource {
     func reloadTableView() {
         print("reloadTableView!")
         myMenualTableView.reloadData()
-        myMenualCollectionView.reloadData()
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
@@ -468,19 +379,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
             let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "DividerView", for: indexPath) as! DividerView
             print("sectionHeader = \(sectionHeader)")
             return sectionHeader
-        // MyMenualCollectionView
-        case 1:
-            if kind == UICollectionView.elementKindSectionFooter {
-                let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "DividerView", for: indexPath) as! DividerView
-                
-                guard let diaryYearModel = listener?.diaryMonthSetRelay.value[safe: indexPath.section] else { return UICollectionReusableView() }
-                
-                let date = diaryYearModel.year.description
-                sectionHeader.divider.dateTitle = date
-                return sectionHeader
-            } else { //No footer in this case but can add option for that
-                 return UICollectionReusableView()
-            }
 
         default:
             return UICollectionReusableView()
@@ -493,10 +391,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
         // MomentsCollectionView
         case 0:
             return CGSize.zero
-
-        // MyMenualCollectionView
-        case 1:
-            return CGSize(width: 24, height: 56)
             
         default:
             return CGSize.zero
@@ -508,13 +402,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
         // MomentsCollectionView
         case 0:
             return 1
-
-        // MyMenualCollectionView
-        case 1:
-            guard let count = listener?.diaryMonthSetRelay.value.count else { return 1 }
-            // 데이터가 하나도 없어도 해당 월 Cell은 표시
-            print("count: \(count)")
-            return count
             
         default:
             return 5
@@ -528,11 +415,7 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
             let momentsCollectionViewCount: Int = 5
             self.momentsCollectionViewPagination.numberOfPages = momentsCollectionViewCount
             return momentsCollectionViewCount
-        // MyMenualCollectionView
-        case 1:
-            guard let diaryYearModel = listener?.diaryMonthSetRelay.value[safe: section] else { return 0 }
-            return diaryYearModel.months?.monthCount ?? 0
-            
+        
         default:
             return 5
         }
@@ -549,30 +432,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
             cell.momentsTitle = "타이틀은 최대 20자를 작성할 수 있습니다. 그 이상일 경우 우오아우아"
             
             return cell
-        // MyMenualCollectionView
-        case 1:
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabsCell", for: indexPath) as? TabsCell else { return UICollectionViewCell() }
-            
-            guard let diaryYearModel = listener?.diaryMonthSetRelay.value[safe: indexPath.section] else { return UICollectionViewCell() }
-            
-            print("!!!!!!! \(indexPath)")
-            guard let countData = diaryYearModel.months?.getArr()[safe: indexPath.row],
-                  let monthData = diaryYearModel.months?.getMonthArr()[safe: indexPath.row] else { return UICollectionViewCell() }
-            
-            // 달 이름
-            let month = monthData
-            let count = countData
-            
-            cell.title = month
-            cell.number = count.description
-            
-            // 첫번째 cell일 경우 active 상태
-            cell.tabsCellStatus = .inactive
-            if indexPath.section == 0 && indexPath.row == 0 {
-                cell.tabsCellStatus = .active
-            }
-            
-            return cell
         default:
             return UICollectionViewCell()
         }
@@ -585,9 +444,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
         case 0:
             let width = UIScreen.main.bounds.width - 40
             return CGSize(width: width, height: 125)
-        // MyMenualCollectionView
-        case 1:
-            return CGSize(width: 75, height: 58)
             
         default:
             return CGSize(width: 32, height: 32)
@@ -599,20 +455,6 @@ extension DiaryHomeViewController: UICollectionViewDelegate, UICollectionViewDel
         // MomentsCollectionView
         case 0:
             print("MomentsCollectionView Selected!")
-        // MyMenualCollectionView
-        case 1:
-            for cell in collectionView.visibleCells {
-                guard let cell = cell as? TabsCell else { return }
-                cell.tabsCellStatus = .inactive
-            }
-            
-            guard let selectedCell = collectionView.cellForItem(at: indexPath) as? TabsCell else { return }
-            selectedCell.tabsCellStatus = .active
-            
-//             self.myMenualTableView.scrollToRow(at: [0, 5], at: .top, animated: true)
-            
-            
-            print("MyMenualCollectionView Selected!")
             
         default:
             return
--- Menual/Profile/ProfileHome/ProfileHomeViewController.swift
+++ Menual/Profile/ProfileHome/ProfileHomeViewController.swift
@@ -8,19 +8,35 @@
 import RIBs
 import RxSwift
 import UIKit
+import SnapKit
 
 protocol ProfileHomePresentableListener: AnyObject {
     func pressedBackBtn()
+}
+
+enum ProfileHomeSection: Int {
+    case SETTING1 = 0
+    case SETTING2 = 1
 }
 
 final class ProfileHomeViewController: UIViewController, ProfileHomePresentable, ProfileHomeViewControllable {
 
     weak var listener: ProfileHomePresentableListener?
     
-    lazy var naviView = MenualNaviView(type: .moments).then {
+    lazy var naviView = MenualNaviView(type: .myPage).then {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.backButton.addTarget(self, action: #selector(pressedBackBtn), for: .touchUpInside)
-        $0.titleLabel.text = MenualString.title_moments
+    }
+    
+    lazy var settingTableView = UITableView(frame: CGRect.zero, style: .grouped).then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .clear
+        $0.contentInset = UIEdgeInsets(top: UIApplication.topSafeAreaHeight, left: 0, bottom: 0, right: 0)
+        $0.sectionHeaderHeight = 34
+        $0.delegate = self
+        $0.dataSource = self
+        $0.register(ProfileHomeCell.self, forCellReuseIdentifier: "ProfileHomeCell")
+        $0.rowHeight = 56
     }
     
     init() {
@@ -35,10 +51,7 @@ final class ProfileHomeViewController: UIViewController, ProfileHomePresentable,
     
     override func viewDidLoad() {
       super.viewDidLoad()
-        view.backgroundColor = .red
-        print("ProfileHome!")
-        
-        title = "MYPAGE"
+        view.backgroundColor = Colors.background
         
         // 뒤로가기 제스쳐 가능하도록
         navigationController?.interactivePopGestureRecognizer?.delegate = nil
@@ -47,6 +60,7 @@ final class ProfileHomeViewController: UIViewController, ProfileHomePresentable,
     
     func setViews() {
         self.view.addSubview(naviView)
+        self.view.addSubview(settingTableView)
         self.view.bringSubviewToFront(naviView)
         
         naviView.snp.makeConstraints { make in
@@ -54,6 +68,10 @@ final class ProfileHomeViewController: UIViewController, ProfileHomePresentable,
             make.height.equalTo(44 + UIApplication.topSafeAreaHeight)
             make.top.equalToSuperview()
             make.width.equalToSuperview()
+        }
+        
+        settingTableView.snp.makeConstraints { make in
+            make.leading.width.top.bottom.equalToSuperview()
         }
     }
     
@@ -66,5 +84,53 @@ final class ProfileHomeViewController: UIViewController, ProfileHomePresentable,
     func pressedBackBtn() {
         print("ProfileHomeVC :: pressedBackBtn!")
         listener?.pressedBackBtn()
+    }
+}
+
+// MARK: - UITableView
+extension ProfileHomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        print("sectionNumber = \(section)")
+        let headerView = ListHeader(type: .datepageandicon, rightIconType: .none)
+        let divider = Divider(type: ._2px)
+        headerView.addSubview(divider)
+        divider.snp.makeConstraints { make in
+            make.top.equalTo(headerView.snp.top).offset(32)
+            make.leading.equalToSuperview().offset(20)
+            make.width.equalToSuperview().inset(20)
+            make.height.equalTo(2)
+        }
+
+        switch section {
+        case ProfileHomeSection.SETTING1.rawValue:
+            headerView.title = "SETTING - 1"
+            return headerView
+        case ProfileHomeSection.SETTING2.rawValue:
+            headerView.title = "SETTING - 2"
+            return headerView
+        default:
+            return UIView()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileHomeCell", for: indexPath) as? ProfileHomeCell else { return UITableViewCell() }
+        
+        if indexPath.row % 2 == 0 {
+            cell.profileHomeCellType = .toggle
+        }
+        
+        cell.title = "안녕하세요 \(indexPath.row)"
+        
+        return cell
     }
 }
