29670d4a3a8011977482c6c036f709ff88a7fa3e
Jin Gyun, Jeong <jjinggu96@gmail.com>
2022-11-27 20:11:19 +0900

    개발자에게 문의하기 추가

--- Menual/Profile/ProfileHome/ProfileHomeViewController.swift
+++ Menual/Profile/ProfileHome/ProfileHomeViewController.swift
@@ -10,6 +10,7 @@ import RxSwift
 import UIKit
 import SnapKit
 import RxRelay
+import MessageUI
 
 protocol ProfileHomePresentableListener: AnyObject {
     func pressedBackBtn(isOnlyDetach: Bool)
@@ -232,10 +233,90 @@ extension ProfileHomeViewController: UITableViewDelegate, UITableViewDataSource 
             } else if data.title == "오픈 소스 라이브러리 보기" {
                 print("ProfileHome :: 오픈 소스 라이브러리 보기 호출!")
                 listener?.pressedProfileOpensourceCell()
+            } else if data.title == "개발자에게 문의하기" {
+                self.pressedDeveloperQACell()
             }
             break
         default:
             break
         }
+    }
+}
+
+// MARK: - MessageUI
+extension ProfileHomeViewController: MFMailComposeViewControllerDelegate {
+    func pressedDeveloperQACell() {
+        print("ProfileHome :: 개발자에게 문의하기!")
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                             이곳에 내용을 작성해주세요.
+                             
+                             오타 발견 문의 시 아래 양식에 맞춰 작성해주세요.
+                             
+                             <예시>
+                             글귀 ID : 글귀 4 (글귀 클릭 시 상단에 표시)
+                             수정 전 : 실수해도 되.
+                             수정 후 : 실수해도 돼.
+                             
+                             -------------------
+                             
+                             Device Model : \(self.getDeviceIdentifier())
+                             Device OS : \(UIDevice.current.systemVersion)
+                             App Version : \(self.getCurrentVersion())
+                             
+                             -------------------
+                             """
+            
+            composeViewController.setToRecipients(["jjikkyu@naver.com"])
+            composeViewController.setSubject("<메뉴얼> 문의 및 의견")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+            
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            print("메일 보내기 실패")
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                // 앱스토어로 이동하기(Mail)
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // Device Identifier 찾기
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+
+    // 현재 버전 가져오기
+    func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
     }
 }
