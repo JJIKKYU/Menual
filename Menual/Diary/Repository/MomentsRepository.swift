0c66aefc669bc94f6b040119def09852ac9c6561
Jin Gyun, Jeong <jjinggu96@gmail.com>
2022-12-03 18:56:43 +0900

    MomentsRepository 추가

---
+++ Menual/Diary/Repository/MomentsRepository.swift
@@ -0,0 +1,20 @@
+//
+//  MomentsRepository.swift
+//  Menual
+//
+//  Created by 정진균 on 2022/12/03.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+import RealmSwift
+import RxRealm
+
+public protocol MomentsRepository {
+    
+}
+
+public final class MomentsRepositoryImp: MomentsRepository {
+    
+}
