9cfc8bd0fab6e387994c6b256257ec2f3bdb4606
JJIKKYU <jjinggu96@gmail.com>
2023-01-28 13:16:26 +0900

    BoxButton Highlight 적용되지 않는 이슈 수정

--- Platform/Sources/DesignSystem/Views/BoxButton/BoxButton.swift
+++ Platform/Sources/DesignSystem/Views/BoxButton/BoxButton.swift
@@ -9,7 +9,6 @@ import Foundation
 import UIKit
 import Then
 import SnapKit
-import DesignSystem
 
 public enum BoxButtonStatus {
     case active
@@ -144,13 +143,13 @@ public class BoxButton: UIButton {
             print("BoxButton :: isSelecteed! - false")
         }
         
-//        if btnStatus == .inactive { return }
-//        switch btnSelected {
-//        case true:
-//            btnStatus = .pressed
-//
-//        case false:
-//            btnStatus = .active
-//        }
+        if btnStatus == .inactive { return }
+        switch btnSelected {
+        case true:
+            btnStatus = .pressed
+
+        case false:
+            btnStatus = .active
+        }
     }
 }
