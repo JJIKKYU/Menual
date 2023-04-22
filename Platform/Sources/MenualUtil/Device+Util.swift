d98b85ab09b6ea5a2da7f841b056748cba4355f8
JJIKKYU <jjinggu96@gmail.com>
2023-04-22 16:30:17 +0900

    앱 버전 및 디바이스 정보 가져오는 함수 Util로 분리

---
+++ Platform/Sources/MenualUtil/Device+Util.swift
@@ -0,0 +1,30 @@
+//
+//  DeviceUtil.swift
+//  
+//
+//  Created by 정진균 on 2023/04/22.
+//
+
+import Foundation
+
+public class DeviceUtil {
+    // Device Identifier 찾기
+    public static func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+
+    // 현재 버전 가져오기
+    public static func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+}
--- Profile/Sources/ProfileHome/ProfileHomeViewController.swift
+++ Profile/Sources/ProfileHome/ProfileHomeViewController.swift
@@ -339,9 +339,9 @@ extension ProfileHomeViewController: MFMailComposeViewControllerDelegate {
                              
                              -------------------
                              
-                             Device Model : \(self.getDeviceIdentifier())
+                             Device Model : \(DeviceUtil.getDeviceIdentifier())
                              Device OS : \(UIDevice.current.systemVersion)
-                             App Version : \(self.getCurrentVersion())
+                             App Version : \(DeviceUtil.getCurrentVersion())
                              
                              -------------------
                              """
@@ -374,26 +374,6 @@ extension ProfileHomeViewController: MFMailComposeViewControllerDelegate {
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         self.dismiss(animated: true, completion: nil)
-    }
-    
-    // Device Identifier 찾기
-    func getDeviceIdentifier() -> String {
-        var systemInfo = utsname()
-        uname(&systemInfo)
-        let machineMirror = Mirror(reflecting: systemInfo.machine)
-        let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
-            return identifier + String(UnicodeScalar(UInt8(value)))
-        }
-        
-        return identifier
-    }
-
-    // 현재 버전 가져오기
-    func getCurrentVersion() -> String {
-        guard let dictionary = Bundle.main.infoDictionary,
-              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
-        return version
     }
 }
 
