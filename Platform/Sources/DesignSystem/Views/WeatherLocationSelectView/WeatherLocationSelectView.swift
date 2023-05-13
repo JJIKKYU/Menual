a70597ec97fd20cdb972767586b07cc316bdf26b
JJIKKYU <jjinggu96@gmail.com>
2023-05-13 15:05:56 +0900

    PlaceModel 삭제 반영

--- Platform/Sources/DesignSystem/Views/WeatherLocationSelectView/WeatherLocationSelectView.swift
+++ Platform/Sources/DesignSystem/Views/WeatherLocationSelectView/WeatherLocationSelectView.swift
@@ -172,20 +172,12 @@ public class WeatherLocationSelectView: UIView {
             switch selectedPlaceType {
             case .place:
                 selectImageView.image = Asset._24px.place.image.withRenderingMode(.alwaysTemplate)
-            case .car:
-                selectImageView.image = Asset._24px.Place.car.image.withRenderingMode(.alwaysTemplate)
             case .company:
                 selectImageView.image = Asset._24px.Place.company.image.withRenderingMode(.alwaysTemplate)
             case .home:
                 selectImageView.image = Asset._24px.Place.home.image.withRenderingMode(.alwaysTemplate)
             case .school:
                 selectImageView.image = Asset._24px.Place.school.image.withRenderingMode(.alwaysTemplate)
-            case .bus:
-                selectImageView.image = Asset._20px.Place.bus.image.withRenderingMode(.alwaysTemplate)
-            case .subway:
-                selectImageView.image = Asset._20px.Place.subway.image.withRenderingMode(.alwaysTemplate)
-            case .store:
-                selectImageView.image = Asset._20px.Place.store.image.withRenderingMode(.alwaysTemplate)
             case .travel:
                 selectImageView.image = Asset._20px.Place.luggage.image.withRenderingMode(.alwaysTemplate)
             }
--- Profile/Sources/ProfileDeveloper/ProfileDeveloperInteractor.swift
+++ Profile/Sources/ProfileDeveloper/ProfileDeveloperInteractor.swift
@@ -83,7 +83,7 @@ public final class ProfileDeveloperInteractor: PresentableInteractor<ProfileDeve
                     switch weatherValue {
                     case 0:
                         weatherModel = WeatherModelRealm(weather: .sun, detailText: "디테일 텍스트입니다.")
-                        placeModel = PlaceModelRealm(place: .car, detailText: "디테일 디테일 텍스트입니다.")
+                        placeModel = PlaceModelRealm(place: .company, detailText: "디테일 디테일 텍스트입니다.")
                     case 1:
                         weatherModel = WeatherModelRealm(weather: .rain, detailText: "디테일 텍스트입니다.")
                         placeModel = PlaceModelRealm(place: .company, detailText: "디테일 디테일 텍스트입니다.")
