5e6e9b3306e414d134106dc1978765e4fcafb286
JJIKKYU <jjinggu96@gmail.com>
2023-05-28 21:32:34 +0900

    Realm Sync 테스트

---
+++ Platform/Sources/MenualRepository/RealmSyncRepository.swift
@@ -0,0 +1,19 @@
+//
+//  RealmSyncRepository.swift
+//  
+//
+//  Created by 정진균 on 2023/05/28.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmSyncRepository {
+    
+    func test() async {
+        let app = App(id: "")
+        let user = app.currentUser
+        let partitionValue = RealmSyncConfigurations.
+    }
+    
+}
